091456ec17a847cf3d8dae3a11aad83e40b8711b
David Nguyen <david.nguyen@terralogic.com>
2022-12-27 13:23:38 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/product_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,26 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My Shop"),
-      ),
-      body: Center(
-        child: Text("Let build the app"),
-      ),
+      home: ProductOverviewScreen(),
     );
   }
 }
