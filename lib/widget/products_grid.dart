864a971242bcc49d1059f5d365aefd1af73c0e66
David Nguyen <david.nguyen@terralogic.com>
2022-12-28 14:41:17 +0700

    provider & listeners

--- lib/widget/products_grid.dart
+++ lib/widget/products_grid.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/widget/product_item.dart';
-
 
 import '../providers/products.dart';
+import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
   const ProductsGrid({Key? key}) : super(key: key);
@@ -14,16 +13,17 @@ class ProductsGrid extends StatelessWidget {
     final products = productsData.items;
 
     return GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: products.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10),
-        itemBuilder: (ctx, i) => ProductItem(
-            id: products[i].id,
-            title: products[i].title,
-            imageUrl: products[i].imageUrl));
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ProductItem(
+          id: products[i].id,
+          title: products[i].title,
+          imageUrl: products[i].imageUrl),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+    );
   }
 }
